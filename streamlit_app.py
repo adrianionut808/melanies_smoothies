8b8972e025a7bed64a4a066565d34adaf46985af
adrianionut808 <adrianionutg00@gmail.com>
2026-05-19 16:47:50 +0300

    Clean up imports and session handling
    Removed unused imports and session initialization.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,6 @@ st.write('The name of your Smoothie will be', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-from snowflake.snowpark.functions import col
-
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
